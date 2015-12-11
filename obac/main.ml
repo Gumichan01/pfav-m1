2af1bd7bc3485281763cca5d06bea0399e3f2df0
Gumichan01 <luxon.jean.pierre@gmail.com>
2015-12-11 12:27:52 +0100

    Modification du main

--- obac/main.ml
+++ obac/main.ml
@@ -1,17 +1,23 @@
 
-open Expr
+open Expr;;
+open Mathexpr;;
 
 (* TODO : to change later *)
 
+
 let something () =
   let example = "5*sqrt(36+x^2)+4*(20-x)" in
-  let e = Parsexpr.expr_of_string "5*sqrt(36+x^2)+4*(20-x)" in
+(*  let e = Parsexpr.expr_of_string "5*sqrt(36+x^2)+4*(20-x)" in*)
+  let e = Parsexpr.expr_of_string example in  
   let s = match e with
     | Op(o,_) -> "operator "^o
     | Var v -> "variable "^v
     | Num i -> "number "^(string_of_int i)
-  in
-  print_string ("We have parsed "^example^" and its tree starts with "^s^"\n")
+  
+  and r = consMathExpr e in
+  print_string ("We have parsed "^example^" and its tree starts with "^s^"\n");
+  print_string("math_expr: "^(print_tree_of_math r)^"\n");;
+
 
 let main =
   print_string "Welcome to Obac 0.01\n";
