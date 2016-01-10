e4851624e75ba3b870315c5670670f801b92d654
Gumichan01 <luxon.jean.pierre@gmail.com>
2016-01-10 18:44:39 +0100

    Nouvelles fonctions à appeler dans le main

--- obac/main.ml
+++ obac/main.ml
@@ -7,7 +7,7 @@ open Plot;;
 
 (* TODO  : to change later *)
 let something () =
-  let example = "pi" in
+  let example = "x +1" in
   (*  let e = Parsexpr.expr_of_string "5*sqrt(36+x^2)+4*(20-x)" in*)
   let e = Parsexpr.expr_of_string example in  
   let s = match e with
@@ -17,13 +17,21 @@ let something () =
       
   and r = cons_math_expr e in
   print_string ("We have parsed "^example^" and its tree starts with "^s^"\n");
+
+  print_string("\nmath_expr: "^(print_tree_of_math r));  
+  print_string("\nFormula: "^(print_formula r)^"\n\n");
   
-  print_string("\nmath_expr: "^(print_tree_of_math r)^"\n\n");
-  
-  let simpl_e = simpl r in 
-  print_string("Simplified math_expr: "^(print_tree_of_math simpl_e)^"\n");
-  print_string("The result of the evaluation is: "^string_of_float(eval simpl_e)^
-		  "\n");
+  let simpl_e = simpl r in
+  let deriv_e = (derive r "x") in
+  let solved_e = (solve r "x") in
+(*  print_string("Simplifed math_expr: "^(print_tree_of_math simpl_e)^"\n");*)
+  print_string("Simplified formula: "^(print_formula simpl_e)^"\n");
+(*  print_string("Derived math_expr: "^(print_tree_of_math deriv_e)^"\n");*)
+  print_string("Derived formula: "^(print_formula deriv_e)^"\n");
+(*  print_string("Solved math_expr: \n");(print_tree_of_math solved_e);*)
+(*  print_string("Solved formula: \n");(print_solve (solved_e));*)
+(*  print_string("The result of the evaluation is: "^string_of_float(eval simpl_e)^
+		  "\n");*)
   
   
   let m="7" in
