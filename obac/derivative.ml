443f36b02a871c9feaa62cdcca02de7f6be50699
Gumichan01 <luxon.jean.pierre@gmail.com>
2016-01-10 21:03:01 +0100

    Intégration de la fonction exponentielle

--- obac/derivative.ml
+++ obac/derivative.ml
@@ -112,7 +112,10 @@ let rec integ : math_expr -> string -> math_expr -> math_expr -> math_expr =
     | Frac(Val(Num.Int(1)),(Binop('*',Val(Num.Int(2)),Sqrt(Var(v)))))
 	when v = s -> Binop('-',Sqrt(b),Sqrt(a))
 
+    (* 1/x -> log(x)*)
     | Frac(Val(Num.Int(1)),Var(v)) 
 	when v = s -> Binop('-', Log(b),Log(a))
+
+    |Expo(Var(v)) when v = s -> Binop('-', Expo(b),Expo(a))
 
     | _ -> failwith "TODO integ : math_expr -> string -> math_expr -> math_expr -> math_expr ";;
